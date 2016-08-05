77518b00b8981d0a8a1f4f8ffa75e214727ad794
Theo Tsapakos <ttsapakos@gmail.com>
2016-08-04 21:12:43 -0400

    Files for Brick Breaker

---
+++ obj/local/armeabi/objs-debug/android_native_app_glue/android_native_app_glue.o.d
@@ -0,0 +1,200 @@
+obj/local/armeabi/objs-debug/android_native_app_glue/android_native_app_glue.o: \
+ /Users/ttsapakos/Android/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.c \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/errno.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/errno.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/errno.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/unistd.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/select.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/signal.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/ucontext.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/user.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysconf.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/capability.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pathconf.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/resource.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/resource.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/resource.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/poll.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/time.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sched.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/configuration.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/asset_manager.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/looper.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_activity.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/input.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/keycodes.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_window.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/rect.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/log.h
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/errno.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/errno.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/errno.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/unistd.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/select.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/user.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysconf.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/capability.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pathconf.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/resource.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/resource.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/resource.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/resource.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/poll.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/time.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sched.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/configuration.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/asset_manager.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/looper.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_activity.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/input.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/keycodes.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_window.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/rect.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/log.h:
---
+++ obj/local/armeabi/objs-debug/hellodroid/main.o.d
@@ -0,0 +1,175 @@
+obj/local/armeabi/objs-debug/hellodroid/main.o: jni/main.cpp \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/jni.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/api-level.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/EGL/egl.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/EGL/eglplatform.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/KHR/khrplatform.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/stdint.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_window.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/rect.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/GLES2/gl2.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/GLES2/gl2platform.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/GLES2/gl2ext.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/sensor.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/stddef.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/compiler.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/kernel.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/looper.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/log.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/poll.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/poll.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/poll.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pthread.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/time.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/time.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/time.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/siginfo.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/signal.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/limits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/page.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/string.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/malloc.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/signal.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/ucontext.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/user.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sched.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/configuration.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/asset_manager.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_activity.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/input.h \
+ /Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/keycodes.h
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/api-level.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/EGL/egl.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/EGL/eglplatform.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/KHR/khrplatform.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/stdint.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_window.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/rect.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/GLES2/gl2.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/GLES2/gl2platform.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/GLES2/gl2ext.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/sensor.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/posix_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/stddef.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/compiler.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/posix_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/kernel.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/sysmacros.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/looper.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/log.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/sources/android/native_app_glue/android_native_app_glue.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/poll.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/poll.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/poll.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/pthread.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/time.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/time.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/time.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/siginfo.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/signal.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/linux/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/internal_types.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/machine/limits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/syslimits.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/page.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/string.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/malloc.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/signal.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm-generic/signal.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/asm/sigcontext.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/ucontext.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sys/user.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/sched.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/configuration.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/asset_manager.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/native_activity.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/input.h:
+
+/Users/ttsapakos/Android/android-ndk-r10c/platforms/android-9/arch-arm/usr/include/android/keycodes.h:
